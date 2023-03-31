487e7301182848a0d0b4b63f8ff05d3831771b72
Sinmysize <117319536+Sinmysize@users.noreply.github.com>
2023-03-31 04:00:38 -0700

    Updated JS

--- menu.js
+++ menu.js
@@ -28,15 +28,65 @@ if ( randomnbum == 0 ) {
 
 //menu
 let menu = document.getElementById('menu')
+let blurA = document.getElementById("blur")
 
 document.getElementById('menuButton').addEventListener('click', ()=> {
 
     if ( opened == false ) {
+
         menu.style.width = '250px'
+        blurA.style.display = "block"
+
+        setTimeout(()=>{
+            blurA.style.opacity = "1"
+        },0)
+
         opened = true
     } else if ( opened == true ) {
+
+        blurA.style.opacity = "0"
         menu.style.width = '0px'
         opened = false
+
+        setTimeout(()=>{
+            blurA.style.display = "none"
+        },200)
     }
     
 })
+
+//clock
+
+function timeSet() {
+    //gets local time
+    let date = new Date()
+    let hh = date.getHours()
+    let mm = date.getMinutes()
+    let ss = date.getSeconds()
+    let session = "AM"
+
+    // Calculates Time
+    if (hh == 0) {
+        hh = 12
+    }
+
+    if (hh > 12) {
+        hh = hh - 12
+        session = "PM"
+    }
+
+    hh = (hh < 10) ? "0" + hh : hh //Note to self: ? is for condition statment
+    mm = (mm < 10) ? "0" + mm : mm
+    ss = (ss < 10) ? "0" + ss : ss
+
+    let time = hh + ':' + mm + ':' + ss + " " + session
+
+    document.getElementById('clock').innerText = time
+    let t = setTimeout(()=>{
+        timeSet()
+    }, 1000)
+}
+
+timeSet()
+
+
